f12a67deef1892cd929ae6011f281926726d4947
righthan <2753875841@qq.com>
2023-08-11 17:17:45 +0800

    修改找不到文件的代码

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
 # Pkg-Insights
 
-分析当前项目目录下的`package.json`出发，遍历分析node_modules下的所有依赖包关系的cli工具
+分析当前项目目录下的 `package.json`出发，遍历分析node_modules下的所有依赖包关系的cli工具
 
 **使用方法**
 
 1. 克隆仓库到本地
-
 2. 进入这个项目的根目录，执行以下命令
 
    ```
+   // 切换到项目目录
+   cd pkg-insights
    // 安装项目所需依赖
    npm install
    // 安装当前项目到全局
    npm install -g
    ```
-
 3. 使用命令行终端或是VSCode中的terminal进入一个需要分析的前端项目文件目录（**确保当前路径层级中含有package.json文件**）
-
 4. 运行以下命令执行工具
 
    ```
@@ -29,4 +28,3 @@
 ![screenshot1](docs/images/screenshot1.png)
 
 ![screenshot2](docs/images/screenshot2.png)
-
--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ program
     chalk.blue("A tool for analyse the npm dependencies of the project")
   )
   .option(
-    "--depth <value>",
+    "-d, --depth <value>",
     "Input the depth of the packages you want to analyse"
   )
   .option("--json <file-path>", "Don't open the website, show me the json file")
@@ -31,8 +31,10 @@ console.log(chalk.blue(figlet.textSync("Pkg - Insights")));
 // 如果命令没有参数则输出命令提示
 if (!process.argv.slice(2).length) {
   program.outputHelp();
-  console.log(chalk.red(`[Error] command options not found, use the options above`));
-  process.exit()
+  console.log(
+    chalk.red(`[Error] command options not found, use the options above`)
+  );
+  process.exit();
 }
 
 // 获取当前工作目录
@@ -49,14 +51,15 @@ fs.readFile(packageJsonPath, "utf8", (err: any, data: any) => {
         `[Error] no package.json file was found in current path: ${cwd}, \n\tmake sure that you have a package.json file in the current working directory`
       )
     );
-    return;
+    // return;
+    process.exit();
   }
   // 打印文件内容
   // console.log('the packages in dependencies:\n',JSON.parse(data).dependencies);
+
+  // 启动服务器
+  const url = runServer();
+
+  // 调用默认浏览器打开网址
+  open(url);
 });
-
-// 启动服务器
-const url = runServer();
-
-// 调用默认浏览器打开网址
-open(url);
